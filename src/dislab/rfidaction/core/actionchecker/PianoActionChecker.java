0b3ce820bf40952438e6ba6f66b8f664748ef471
rfid <342624757@qq.com>
2017-06-09 14:56:44 +0800

    pinano check

--- src/dislab/rfidaction/core/actionchecker/PianoActionChecker.java
+++ src/dislab/rfidaction/core/actionchecker/PianoActionChecker.java
@@ -15,76 +15,80 @@ public class PianoActionChecker extends AbstractActionChecker {
 	/**
 	 * 每根手指对应的阈值，最好每次都根据pinao.csv数据调一下
 	 */
-	private double[] threholds = new double[]{
-		48,48,43,42,48,48,48,49,50,50	
-	};
+	private double[] threholds = new double[] { 47, 47, 45, 42, 48, 48, 48, 49, 50, 50 };
 
 	public PianoActionChecker(List<String> tagIds) {
 		super(tagIds);
 		System.out.println("PianoActionChecker created");
-		tagPressed = new int[tagIds.size()];//以tagIds的大小确定数组大小
+		tagPressed = new int[tagIds.size()];// 以tagIds的大小确定数组大小
 	}
 
 	@Override
 	public String checkAction(double[] tagRssi) {
 		for (int i = 1; i < 4; i++) {
 			matchPianoAction(tagRssi, i, i - 1);
-		}//2、3、4
+		} // 2、3、4
 		for (int i = 6; i < 10; i++) {
 			matchPianoAction(tagRssi, i, i - 3);
-		}//7、8、9、10
-		// TODO Auto-generated method stub
+		} // 7、8、9、10
+			// TODO Auto-generated method stub
 		return null;
 
 	}
 
 	/**
 	 * 匹配钢琴动作
+	 * 
 	 * @param tagRssi
-	 * 标签的RSSI
+	 *            标签的RSSI
 	 * @param index
-	 * 标签对应的索引
+	 *            标签对应的索引
 	 * @param key
-	 * 钢琴键与index对应
+	 *            钢琴键与index对应
 	 */
 	private void matchPianoAction(double[] tagRssi, int index, int key) {
 		double rssi = tagRssi[index];
-		double threholdPress = threholds[index] ;
-		//		if(index!=3&&index!=2) {
-//			threholdPress = threholdRress = threholdPressPinao;
-//		}
-		if (rssi > threholdPress ) {
-			tagPressed[index] = 2;
+		double threholdPress = threholds[index];
+		// if(index!=3&&index!=2) {
+		// threholdPress = threholdRress = threholdPressPinao;
+		// }
+		if (rssi > threholdPress && tagPressed[index] != 1) {
+
 			new PianoCommand(new CommandInfo(CommandInfo.PRESS, key)).excute();
-			new Thread(new Runnable() {
-				
-				@Override
-				public void run() {
-//					try {
-//						Thread.sleep(500);
-//						tagPressed[index] = 0;
-//					} catch (InterruptedException e1) {
-//						// TODO Auto-generated catch block
-//						e1.printStackTrace();
-//					}
-					try {
-						Thread.sleep(800);
-					} catch (InterruptedException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
-//					if(tagPressed[index] ==0)
-					new PianoCommand(new CommandInfo(CommandInfo.RELEASE, key))
-					.excute();	
-				}
-			}).start();
-		} 
-//		else if ( tagPressed[index]!=0) {
-//			tagPressed[index]--;
-//			if(tagPressed[index]==0){
-//				
-//			}
-//		}
+			tagPressed[index] = 1;
+
+			// new Thread(new Runnable() {
+			//
+			// @Override
+			// public void run() {
+			//// try {
+			//// Thread.sleep(500);
+			//// tagPressed[index] = 0;
+			//// } catch (InterruptedException e1) {
+			//// // TODO Auto-generated catch block
+			//// e1.printStackTrace();
+			//// }
+			// try {
+			// Thread.sleep(800);
+			// } catch (InterruptedException e) {
+			// // TODO Auto-generated catch block
+			// e.printStackTrace();
+			// }
+			//// if(tagPressed[index] ==0)
+			// new PianoCommand(new CommandInfo(CommandInfo.RELEASE, key))
+			// .excute();
+			// }
+			// }).start();
+		} else if (rssi < threholdPress && tagPressed[index] == 1) {
+			new PianoCommand(new CommandInfo(CommandInfo.RELEASE, key)).excute();
+			tagPressed[index] = 0;
+		}
+		// else if ( tagPressed[index]!=0) {
+		// tagPressed[index]--;
+		// if(tagPressed[index]==0){
+		//
+		// }
+		// }
 	}
 
 	@Override
